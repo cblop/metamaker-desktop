65df2dca1a83909b2e771c1609245b093d0b4255
cblop <commanderblop@gmail.com>
2017-08-15 09:45:32 +0100

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to metamaker-desktop
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/metamaker_desktop/core_test.clj
@@ -0,0 +1,7 @@
+(ns metamaker-desktop.core-test
+  (:require [clojure.test :refer :all]
+            [metamaker-desktop.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
